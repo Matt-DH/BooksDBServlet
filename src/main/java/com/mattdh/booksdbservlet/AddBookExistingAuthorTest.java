a4cb804f5335c2a73c0d3f629e43b51f3456fe2f
mattdh <mattdavidhardy@gmail.com>
2023-03-20 13:29:51 -0230

    First commit

---
+++ src/main/java/com/mattdh/booksdbservlet/AddBookExistingAuthorTest.java
@@ -0,0 +1,9 @@
+package com.mattdh.booksdbservlet;
+
+public class AddBookExistingAuthorTest {
+
+//    public static void main(String[] args) {
+//        BookDatabaseManager.addBook(DBConfiguration.getBookDBConnection(),"a", 3);
+//    }
+
+}
